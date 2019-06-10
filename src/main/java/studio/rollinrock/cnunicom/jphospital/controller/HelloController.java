a3075a829dcc7eb3a2aaaf92259358e70acfdafa
rollinrock <caojing9111@outlook.com>
2019-06-10 12:35:24 +0800

    删除相关依赖代码：travis默认拉取中央库依赖，无法识别自定义依赖，暂时去除

--- src/main/java/studio/rollinrock/cnunicom/jphospital/controller/HelloController.java
+++ src/main/java/studio/rollinrock/cnunicom/jphospital/controller/HelloController.java
@@ -3,15 +3,14 @@ package studio.rollinrock.cnunicom.jphospital.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import studio.rollinrock.cnunicom.wrappers.HttpResult;
 
 @RestController
 @RequestMapping("/hello")
 public class HelloController {
 
     @GetMapping(value = "/greet")
-    public HttpResult<String> greet() {
-        return HttpResult.succeedWithData("hello u.");
+    public String greet() {
+        return "hello u.";
     }
 
 }
